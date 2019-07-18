65f7b9a53a7511c64c75a34e2f90d4a62c9eb5ce
viniciussouza70 <viniciussouza70@gmail.com>
2019-07-17 22:24:05 -0300

    aula1

---
+++ app/js/controllers/NegociacaoController.js
@@ -0,0 +1,14 @@
+class NegociacaoController {
+    constructor() {
+        //pegando os dados do DOM
+        this._inputData = document.querySelector('#data');
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputValor = document.querySelector('#valor');
+    }
+    adiciona(event) {
+        event.preventDefault();
+        //passando exatamente o que está nos campos do form 
+        const negociacao = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
+        console.log(negociacao);
+    }
+}
---
+++ app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,26 @@
+class NegociacaoController {
+    private _inputData;
+    private _inputQuantidade;
+    private _inputValor;
+
+    constructor(){
+        //pegando os dados do DOM
+        this._inputData = document.querySelector('#data');
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputValor = document.querySelector('#valor');
+    }
+
+    
+    adiciona(event) {
+        
+        event.preventDefault();
+        //passando exatamente o que está nos campos do form 
+        const negociacao = new Negociacao(
+            this._inputData.value,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+    }
+}
